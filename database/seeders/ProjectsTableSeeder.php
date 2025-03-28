447addc3a5d278f5ee45ede7b15092696de80528
danieleBattistelli <bomber81@gmail.com>
2025-03-28 17:44:21 +0100

    feat: add project's seeder

---
+++ database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $newProject = new Project();
+
+            $newProject->name = $faker->sentence();
+            $newProject->client = $faker->name();
+            $newProject->start_date = $faker->dateTimeBetween('-1 year', 'now');
+            $newProject->end_date = $faker->dateTimeBetween('now', '+1 year');
+            $newProject->description = $faker->paragraph(12);
+            
+            $newProject->created_at = $faker->dateTimeBetween('-1 year', 'now');
+            $newProject->updated_at = $faker->dateTimeBetween('-1 year', 'now');
+
+            $newProject->save();
+        }
+
+    }
+}
